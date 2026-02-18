a52d362d11fd0d1fb8c316eca9c3a18ac0df6c8c
Santeri Korpela <sjkorpela03@gmail.com>
2026-02-18 12:29:56 +0200

    Add spring boot maven project

---
+++ src/main/java/com/hecla/heclaBackend/HeclaBackendApplication.java
@@ -0,0 +1,12 @@
+package com.hecla.heclaBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeclaBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HeclaBackendApplication.class, args);
+  }
+}
---
+++ src/test/java/com/hecla/heclaBackend/HeclaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hecla.heclaBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeclaBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+
+  }
+}
