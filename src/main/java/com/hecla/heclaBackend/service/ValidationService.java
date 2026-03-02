5cb55d050838a5cac7545605bcb6e4c037b4fdc5
Santeri Korpela <sjkorpela03@gmail.com>
2026-03-02 17:32:40 +0200

    Update ValidationService

--- src/main/java/com/hecla/heclaBackend/service/ValidationService.java
+++ src/main/java/com/hecla/heclaBackend/service/ValidationService.java
@@ -1,23 +1,32 @@
 package com.hecla.heclaBackend.service;
 
 import com.hecla.heclaBackend.model.DataTransferPerson;
-import com.hecla.heclaBackend.repository.PersonRepo;
+import com.hecla.heclaBackend.model.DocumentPerson;
 import org.apache.coyote.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Arrays;
+import java.util.Map;
 
 @Service
 public class ValidationService {
 
   @Autowired
-  private PersonRepo repo;
+  MongoTemplate repo;
 
   public void validateDataTransferPerson(DataTransferPerson person) throws BadRequestException {
-    if (person.fatherId() != null && !repo.existsById(person.fatherId())) {
+
+    if (person.fatherId() != null && !personExistsById(person.fatherId())) {
       throw new BadRequestException("Given father not found!");
     }
 
-    if (person.motherId() != null && !repo.existsById(person.motherId())) {
+    if (person.motherId() != null && !personExistsById(person.motherId())) {
       throw new BadRequestException("Given mother not found!");
     }
 
@@ -38,5 +47,9 @@ public class ValidationService {
     // Last names validation
 
     // Additional info validation
+  }
+
+  public boolean personExistsById(int id) {
+    return repo.exists(new Query(Criteria.where("_id").is(id)), DocumentPerson.class);
   }
 }
