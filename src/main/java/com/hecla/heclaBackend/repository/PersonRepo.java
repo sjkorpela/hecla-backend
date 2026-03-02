62f543e084282f11b20f3940b8297d5a50a16256
Santeri Korpela <sjkorpela03@gmail.com>
2026-03-02 17:42:25 +0200

    Fix typo

--- src/main/java/com/hecla/heclaBackend/repository/PersonRepo.java
+++ src/main/java/com/hecla/heclaBackend/repository/PersonRepo.java
@@ -1,8 +1,10 @@
 package com.hecla.heclaBackend.repository;
 
-import com.hecla.heclaBackend.model.Person;
+import com.hecla.heclaBackend.model.DocumentPerson;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface PersonRepo extends MongoRepository<Person, Integer> {
+import javax.swing.text.Document;
+
+public interface PersonRepo extends MongoRepository<DocumentPerson, Integer> {
 
 }
